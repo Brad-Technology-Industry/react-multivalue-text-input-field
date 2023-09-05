0b483bd9c6b072aff90a8bb1ed451a6b6382fe9f
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-05 04:49:56 +0100

    feat(app): setting prettier and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "type-enum": [
+        2,
+        "always",
+        [
+          "build",
+          "chore",
+          "ci",
+          "merge",
+          "docs",
+          "improvement",
+          "feat",
+          "fix",
+          "perf",
+          "refactor",
+          "revert",
+          "style",
+          "test",
+        ],
+      ],
+    },
+};
+  
