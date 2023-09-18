e4d90350225c1bd32158d2d2ca7867c94de4008b
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-18 10:17:49 +0100

    feat(app): added test suit with jest

---
+++ src/tests/common.test.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+
+import 'jest-canvas-mock';
+import MultiInputField from '..';
+
+describe('Common render', () => {
+   it('renders without crashing', () => {
+      render(
+         <MultiInputField
+            label={''}
+            placeholder={''}
+            values={[]}
+            setValues={() => {}}
+         />,
+      );
+   });
+});
