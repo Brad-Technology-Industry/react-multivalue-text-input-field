3cbec1187ea6b15423c19f11ae4d5bf332c7df68
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-19 10:43:52 +0100

    refactor: rearranged file structure

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
-import MultiInputField from '..';
+import MultiInputField from '../src';
 
 describe('Common render', () => {
    it('renders without crashing', () => {
